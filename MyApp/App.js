d999b246b44b3735ce223192435eb9b3633eb6c4
dem0saic <vowusu032@st.ug.edu.gh>
2024-06-02 12:33:21 +0000

    Second component completed

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,Image, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet,Image, Text,TextInput, View, SafeAreaView, TouchableOpacity } from 'react-native';
 
 
 export default function App() {
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#fceeeb'}}>
       <View style={styles.container}>
+
+        {/**First Component */}
         <View style={firs1.firstCompo}>
           <View style={{}}>
             <Text style={{fontSize: 32, fontWidth: 'bold', font: ''}}>Hello, Devs</Text>
@@ -15,6 +17,21 @@ export default function App() {
           <TouchableOpacity style={{backgroundColor:'#fff', padding: 6.5, borderRadius: 30,}}>
               <Image source={require("./assets/profile.png")}></Image>
           </TouchableOpacity>
+        </View>
+
+        {/**Second Component */}  
+        <View style={sec2.secondCompo}>
+          {/**TextInput Box  */}
+          <TextInput placeholder="Search" style={searchInput.searchHere}/>
+    
+          {/**Search Icon and FIlter Icon */}
+          <View style={{backgroundColor: "#F0522F", padding: 10, borderRadius: 10}}>
+            <Image source={require("./assets/Vector.png")} />
+          </View>
+        </View>
+
+        <View style={{gap: 10,}}>
+          <Text style={{fontSize: 20, fontWeight: "bold", top: 55,}}>Categories</Text>
         </View>
           <StatusBar style="auto" />
       </View>
@@ -32,6 +49,7 @@ const styles = StyleSheet.create({
   },
 });
 
+{/**First Component */}
 const firs1 = StyleSheet.create({
   firstCompo: {
     display: "flex", 
@@ -43,5 +61,32 @@ const firs1 = StyleSheet.create({
     top: 42,
     gap: 134,
     left: 2,
+  }
+})
+
+  {/**Second Component */}  
+const sec2 = StyleSheet.create({
+  secondCompo: {
+    display: "flex", 
+    flexDirection: "row", 
+    alignItems: "center", 
+    justifyContent: "space-between",
+    width: 380,
+    height: 49,
+    top: 55,
+    left: 2,
+    border: 1,
+  }
+})
+
+{/** SearchInput Area */}
+const searchInput = StyleSheet.create({
+  searchHere: {
+    backgroundColor: "white",
+    width: 300,
+    height: 48,
+    borderRadius: 10,
+    padding: 5,
+    border: 1,
   }
 })
